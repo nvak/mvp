112557181955dbeede4ceac5bc87070427d73c17
Nickolai Vakulich <nickolai.vakulich@gmail.com>
2022-07-17 17:15:13 +0300

    remove lombok dependency

--- src/main/java/mvp/model/GameResult.java
+++ src/main/java/mvp/model/GameResult.java
@@ -1,10 +1,51 @@
 package mvp.model;
 
 import java.util.List;
-import lombok.Data;
+import java.util.Objects;
 
-@Data
 public class GameResult {
     private Game game;
     private List<String> playerResults;
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    public List<String> getPlayerResults() {
+        return playerResults;
+    }
+
+    public void setPlayerResults(List<String> playerResults) {
+        this.playerResults = playerResults;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        GameResult that = (GameResult) o;
+        return game == that.game
+                && Objects.equals(playerResults, that.playerResults);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(game, playerResults);
+    }
+
+    @Override
+    public String toString() {
+        return "GameResult{"
+                + "game=" + game
+                + ", playerResults=" + playerResults
+                + '}';
+    }
 }
--- src/main/java/mvp/model/Player.java
+++ src/main/java/mvp/model/Player.java
@@ -1,10 +1,55 @@
 package mvp.model;
 
-import lombok.Value;
+import java.util.Objects;
 
-@Value
-public class Player {
-    String name;
-    String nickname;
-    int number;
+public final class Player {
+    private final String name;
+    private final String nickname;
+    private final int number;
+
+    public Player(String name, String nickname, int number) {
+        this.name = name;
+        this.nickname = nickname;
+        this.number = number;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public String nickname() {
+        return nickname;
+    }
+
+    public int number() {
+        return number;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (obj == null || obj.getClass() != this.getClass()) {
+            return false;
+        }
+        var that = (Player) obj;
+        return Objects.equals(this.name, that.name) &&
+                Objects.equals(this.nickname, that.nickname) &&
+                this.number == that.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, nickname, number);
+    }
+
+    @Override
+    public String toString() {
+        return "Player[" +
+                "name=" + name + ", " +
+                "nickname=" + nickname + ", " +
+                "number=" + number + ']';
+    }
+
 }
